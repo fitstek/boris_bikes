25c667bff1b32ff44c18f08e49c04269ccd08552
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2014-09-02 22:18:49 +0100

    de;eted some unncessary comments

--- lib/van.rb
+++ lib/van.rb
@@ -3,8 +3,6 @@ class Van
 
 	include BikeContainer
 
-	# When initializing the Docking Station, if it is given an options hash with
-	# :capacity as a key it takes that value. Otherwise it takes DEFAULT_CAPACITY
 	def initialize(options = {})
 		self.capacity = options.fetch(:capacity, capacity)
 	end
